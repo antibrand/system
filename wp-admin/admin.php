424ba639e7a32968db2f1b3d14aef0bef6acd1d6
ControlledChaos <greg@ccdzine.com>
2019-11-03 12:54:56 -0800

    Added check for APP_NAME constant to admin.php.

--- wp-admin/admin.php
+++ wp-admin/admin.php
@@ -15,20 +15,28 @@ if ( ! defined( 'WP_ADMIN' ) ) {
 	define( 'WP_ADMIN', true );
 }
 
-if ( ! defined('WP_NETWORK_ADMIN') )
-	define('WP_NETWORK_ADMIN', false);
-
-if ( ! defined('WP_USER_ADMIN') )
-	define('WP_USER_ADMIN', false);
-
-if ( ! WP_NETWORK_ADMIN && ! WP_USER_ADMIN ) {
-	define('WP_BLOG_ADMIN', true);
+if ( ! defined( 'WP_NETWORK_ADMIN' ) ) {
+	define( 'WP_NETWORK_ADMIN', false );
 }
 
-if ( isset($_GET['import']) && !defined('WP_LOAD_IMPORTERS') )
-	define('WP_LOAD_IMPORTERS', true);
+if ( ! defined( 'WP_USER_ADMIN' ) ) {
+	define( 'WP_USER_ADMIN', false );
+}
 
-require_once(dirname(dirname(__FILE__)) . '/wp-load.php');
+if ( ! WP_NETWORK_ADMIN && ! WP_USER_ADMIN ) {
+	define( 'WP_BLOG_ADMIN', true );
+}
+
+// Define the application name.
+if ( ! defined( 'APP_NAME' ) ) {
+	define( 'APP_NAME', 'App Name' );
+}
+
+if ( isset( $_GET['import'] ) && ! defined( 'WP_LOAD_IMPORTERS' ) ) {
+	define( 'WP_LOAD_IMPORTERS', true );
+}
+
+require_once( dirname( dirname( __FILE__ ) ) . '/wp-load.php' );
 
 nocache_headers();
 
