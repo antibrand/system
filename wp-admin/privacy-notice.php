b404f534a3f86107a5f88be6e01f85a49cab1108
ControlledChaos <greg@ccdzine.com>
2019-11-03 12:54:04 -0800

    Removed about pages content.

--- wp-admin/privacy-notice.php
+++ wp-admin/privacy-notice.php
@@ -1,20 +1,21 @@
 <?php
 /**
- * About This Version administration panel.
+ * Your Rights administration panel.
  *
  * @package WMS
  * @subpackage Administration
+ *
+ * @todo Edit text when connection to WordPress is complete.
  */
 
 /** WordPress Administration Bootstrap */
 require_once( dirname( __FILE__ ) . '/admin.php' );
 
-wp_enqueue_script( 'underscore' );
-
-$title   = __( 'About' );
+$title   = __( 'Privacy Notice' );
 $version = get_bloginfo( 'version' );
 
 include( ABSPATH . 'wp-admin/admin-header.php' );
+
 ?>
 <div class="wrap not-using__about-wrap not-using__full-width-layout">
 
@@ -22,11 +23,9 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	<p class="description not-using__about-text"><?php printf( __( '' ) ); ?></p>
 
 	<h2 class="nav-tab-wrapper wp-clearfix">
-		<a href="about.php" class="nav-tab nav-tab-active"><?php _e( 'About' ); ?></a>
-		<a href="privacy-notice.php" class="nav-tab"><?php _e( 'Privacy' ); ?></a>
+		<a href="about.php" class="nav-tab"><?php _e( 'About' ); ?></a>
+		<a href="privacy-notice.php" class="nav-tab nav-tab-active"><?php _e( 'Privacy' ); ?></a>
 	</h2>
 
 </div>
-<?php
-
-include( ABSPATH . 'wp-admin/admin-footer.php' );
+<?php include( ABSPATH . 'wp-admin/admin-footer.php' ); ?>
