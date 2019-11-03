f8b52fbe44253252e4eaefd2b2fca978d096bdc8
ControlledChaos <greg@ccdzine.com>
2019-11-03 09:10:11 -0800

    Sample config: Reformatted, changed database prefix, added APP_NAME definition, DEBUG true.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The base configuration for WordPress
+ * The base configuration for the website management system
  *
  * The wp-config.php creation script uses this file during the
  * installation. You don't have to use the web site, you can
@@ -13,77 +13,88 @@
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
  * @package WMS
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+/**
+ * MySQL settings
+ *
+ * You can get this info from your web host.
+ */
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+// Database name.
+define( 'DB_NAME', 'database_name_here' );
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+// Database username.
+define( 'DB_USER', 'username_here' );
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+// Database password.
+define( 'DB_PASSWORD', 'password_here' );
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+// Database hostname.
+define( 'DB_HOST', 'localhost' );
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+// Database charset to use in creating database tables.
+define( 'DB_CHARSET', 'utf8' );
 
-/**#@+
+// Database collate type. Don't change this if in doubt.
+define( 'DB_COLLATE', '' );
+
+/**
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
- * @since 2.6.0
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
-/**#@-*/
+define( 'AUTH_KEY',         'Put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'Put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'Put your unique phrase here' );
+define( 'NONCE_KEY',        'Put your unique phrase here' );
+define( 'AUTH_SALT',        'Put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'Put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'Put your unique phrase here' );
+define( 'NONCE_SALT',       'Put your unique phrase here' );
 
 /**
- * WordPress Database Table prefix.
+ * Database table prefix
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'app_';
 
 /**
- * For developers: WordPress debugging mode.
+ * Define the application name
+ *
+ * This constant is used throughout the website management system
+ * and allows you to "white label" the application.
+ */
+define( 'APP_NAME', 'website management system' );
+
+/**
+ * For developers: debugging mode.
  *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define( 'WP_DEBUG', true );
 
-/* That's all, stop editing! Happy blogging. */
+// Multisite definitions.
+define( 'WP_ALLOW_MULTISITE', false );
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+/**
+ * End customization
+ *
+ * Do not add or edit anything below this comment block.
+ */
 
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+// Absolute path to the app directory.
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+// Sets up vars and included files.
+require_once( ABSPATH . 'wp-settings.php' );
