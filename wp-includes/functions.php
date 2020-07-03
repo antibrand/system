6c45732c6a04e53e6bc03ed31dcb31d00ddeff0d
antibrand <antibrand@ccdzine.com>
2020-07-02 19:12:47 -0800

    Added app-views directory to the guess URL function.

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -4196,42 +4196,57 @@ function force_ssl_admin( $force = null ) {
  * @return string The guessed URL.
  */
 function wp_guess_url() {
-	if ( defined('WP_SITEURL') && '' != WP_SITEURL ) {
+
+	if ( defined( 'WP_SITEURL' ) && '' != WP_SITEURL ) {
 		$url = WP_SITEURL;
+
 	} else {
-		$abspath_fix = str_replace( '\\', '/', ABSPATH );
+
+		$abspath_fix         = str_replace( '\\', '/', ABSPATH );
 		$script_filename_dir = dirname( $_SERVER['SCRIPT_FILENAME'] );
 
-		// The request is for the admin
-		if ( strpos( $_SERVER['REQUEST_URI'], 'wp-admin' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) !== false ) {
+		// The request is for the app-views directory.
+		if ( strpos( $_SERVER['REQUEST_URI'], 'app-views' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) !== false ) {
+			$path = preg_replace( '#/(app-views/.*|wp-login.php)#i', '', $_SERVER['REQUEST_URI'] );
+
+		// The request is for the wp-admin directory.
+		} elseif ( strpos( $_SERVER['REQUEST_URI'], 'wp-admin' ) !== false || strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) !== false ) {
 			$path = preg_replace( '#/(wp-admin/.*|wp-login.php)#i', '', $_SERVER['REQUEST_URI'] );
 
-		// The request is for a file in ABSPATH
+		// The request is for a file in ABSPATH.
 		} elseif ( $script_filename_dir . '/' == $abspath_fix ) {
-			// Strip off any file/query params in the path
+
+			// Strip off any file/query params in the path.
 			$path = preg_replace( '#/[^/]*$#i', '', $_SERVER['PHP_SELF'] );
 
 		} else {
+
 			if ( false !== strpos( $_SERVER['SCRIPT_FILENAME'], $abspath_fix ) ) {
+
 				// Request is hitting a file inside ABSPATH
 				$directory = str_replace( ABSPATH, '', $script_filename_dir );
+
 				// Strip off the sub directory, and any file/query params
 				$path = preg_replace( '#/' . preg_quote( $directory, '#' ) . '/[^/]*$#i', '' , $_SERVER['REQUEST_URI'] );
+
 			} elseif ( false !== strpos( $abspath_fix, $script_filename_dir ) ) {
+
 				// Request is hitting a file above ABSPATH
 				$subdirectory = substr( $abspath_fix, strpos( $abspath_fix, $script_filename_dir ) + strlen( $script_filename_dir ) );
+
 				// Strip off any file/query params from the path, appending the sub directory to the installation
 				$path = preg_replace( '#/[^/]*$#i', '' , $_SERVER['REQUEST_URI'] ) . $subdirectory;
+
 			} else {
 				$path = $_SERVER['REQUEST_URI'];
 			}
 		}
 
 		$schema = is_ssl() ? 'https://' : 'http://'; // set_url_scheme() is not defined yet
-		$url = $schema . $_SERVER['HTTP_HOST'] . $path;
+		$url    = $schema . $_SERVER['HTTP_HOST'] . $path;
 	}
 
-	return rtrim($url, '/');
+	return rtrim( $url, '/' );
 }
 
 /**
