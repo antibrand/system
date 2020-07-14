8061c5775890b1c906c01ce0d8793b6663215d65
antibrand <antibrand@ccdzine.com>
2020-07-14 10:54:14 -0800

    Add browser & OS classes to front end and back end body elements.

--- wp-includes/vars.php
+++ wp-includes/vars.php
@@ -15,125 +15,166 @@
  * @package App_Package
  */
 
-global $pagenow,
-	$is_lynx, $is_gecko, $is_winIE, $is_macIE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone, $is_IE, $is_edge,
-	$is_apache, $is_IIS, $is_iis7, $is_nginx;
+global
+$pagenow,
+$is_lynx,
+$is_gecko,
+$is_winIE,
+$is_macIE,
+$is_opera,
+$is_NS4,
+$is_safari,
+$is_chrome,
+$is_iphone,
+$is_IE,
+$is_edge,
+$is_apache,
+$is_IIS,
+$is_iis7,
+$is_nginx;
 
-// On which page are we ?
+// Admin pages.
 if ( is_admin() ) {
-	// wp-admin pages are checked more carefully
-	if ( is_network_admin() )
-		preg_match('#/wp-admin/network/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches);
-	elseif ( is_user_admin() )
-		preg_match('#/wp-admin/user/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches);
-	else
-		preg_match('#/wp-admin/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches);
+
+	// Admin pages are checked more carefully.
+	if ( is_network_admin() ) {
+		preg_match( '#/wp-admin/network/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches );
+	} elseif ( is_user_admin() ) {
+		preg_match( '#/wp-admin/user/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches );
+	} else {
+		preg_match( '#/wp-admin/?(.*?)$#i', $_SERVER['PHP_SELF'], $self_matches );
+	}
+
 	$pagenow = $self_matches[1];
-	$pagenow = trim($pagenow, '/');
-	$pagenow = preg_replace('#\?.*?$#', '', $pagenow);
+	$pagenow = trim( $pagenow, '/' );
+	$pagenow = preg_replace( '#\?.*?$#', '', $pagenow);
+
 	if ( '' === $pagenow || 'index' === $pagenow || 'index.php' === $pagenow ) {
 		$pagenow = 'index.php';
-	} else {
-		preg_match('#(.*?)(/|$)#', $pagenow, $self_matches);
-		$pagenow = strtolower($self_matches[1]);
-		if ( '.php' !== substr($pagenow, -4, 4) )
-			$pagenow .= '.php'; // for Options +Multiviews: /wp-admin/themes/index.php (themes.php is queried)
-	}
-} else {
-	if ( preg_match('#([^/]+\.php)([?/].*?)?$#i', $_SERVER['PHP_SELF'], $self_matches) )
-		$pagenow = strtolower($self_matches[1]);
-	else
-		$pagenow = 'index.php';
-}
-unset($self_matches);
 
-// Simple browser detection
+	} else {
+
+		preg_match( '#(.*?)(/|$)#', $pagenow, $self_matches );
+		$pagenow = strtolower( $self_matches[1] );
+
+		// For Options +Multiviews: /wp-admin/themes/index.php (themes.php is queried).
+		if ( '.php' !== substr( $pagenow, -4, 4 ) ) {
+			$pagenow .= '.php';
+		}
+	}
+
+} else {
+
+	if ( preg_match( '#([^/]+\.php)([?/].*?)?$#i', $_SERVER['PHP_SELF'], $self_matches ) ) {
+		$pagenow = strtolower( $self_matches[1] );
+	} else {
+		$pagenow = 'index.php';
+	}
+}
+unset( $self_matches );
+
+// Simple browser detection.
 $is_lynx = $is_gecko = $is_winIE = $is_macIE = $is_opera = $is_NS4 = $is_safari = $is_chrome = $is_iphone = $is_edge = false;
 
-if ( isset($_SERVER['HTTP_USER_AGENT']) ) {
-	if ( strpos($_SERVER['HTTP_USER_AGENT'], 'Lynx') !== false ) {
+if ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
+
+	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Lynx' ) !== false ) {
 		$is_lynx = true;
 	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Edge' ) !== false ) {
 		$is_edge = true;
-	} elseif ( stripos($_SERVER['HTTP_USER_AGENT'], 'chrome') !== false ) {
+	} elseif ( stripos( $_SERVER['HTTP_USER_AGENT'], 'chrome' ) !== false ) {
+
 		if ( stripos( $_SERVER['HTTP_USER_AGENT'], 'chromeframe' ) !== false ) {
+
 			$is_admin = is_admin();
 			/**
 			 * Filters whether Google Chrome Frame should be used, if available.
 			 *
-			 * @since 3.2.0
-			 *
+			 * @since Previous 3.2.0
 			 * @param bool $is_admin Whether to use the Google Chrome Frame. Default is the value of is_admin().
 			 */
-			if ( $is_chrome = apply_filters( 'use_google_chrome_frame', $is_admin ) )
+			if ( $is_chrome = apply_filters( 'use_google_chrome_frame', $is_admin ) ) {
 				header( 'X-UA-Compatible: chrome=1' );
+			}
+
 			$is_winIE = ! $is_chrome;
+
 		} else {
 			$is_chrome = true;
 		}
-	} elseif ( stripos($_SERVER['HTTP_USER_AGENT'], 'safari') !== false ) {
+
+	} elseif ( stripos( $_SERVER['HTTP_USER_AGENT'], 'safari' ) !== false ) {
 		$is_safari = true;
-	} elseif ( ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false ) && strpos($_SERVER['HTTP_USER_AGENT'], 'Win') !== false ) {
+
+	} elseif ( ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false || strpos( $_SERVER['HTTP_USER_AGENT'], 'Trident' ) !== false ) && strpos( $_SERVER['HTTP_USER_AGENT'], 'Win' ) !== false ) {
 		$is_winIE = true;
-	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false ) {
+
+	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false && strpos( $_SERVER['HTTP_USER_AGENT'], 'Mac' ) !== false ) {
 		$is_macIE = true;
-	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Gecko') !== false ) {
+
+	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Gecko' ) !== false ) {
 		$is_gecko = true;
-	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false ) {
+
+	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera' ) !== false ) {
 		$is_opera = true;
-	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Nav') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Mozilla/4.') !== false ) {
+
+	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Nav' ) !== false && strpos( $_SERVER['HTTP_USER_AGENT'], 'Mozilla/4.' ) !== false ) {
 		$is_NS4 = true;
 	}
 }
 
-if ( $is_safari && stripos($_SERVER['HTTP_USER_AGENT'], 'mobile') !== false )
+if ( $is_safari && stripos( $_SERVER['HTTP_USER_AGENT'], 'mobile' ) !== false ) {
 	$is_iphone = true;
+}
 
 $is_IE = ( $is_macIE || $is_winIE );
 
-// Server detection
-
 /**
  * Whether the server software is Apache or something else
+ *
  * @global bool $is_apache
  */
-$is_apache = (strpos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false || strpos($_SERVER['SERVER_SOFTWARE'], 'LiteSpeed') !== false);
+$is_apache = ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Apache' ) !== false || strpos( $_SERVER['SERVER_SOFTWARE'], 'LiteSpeed' ) !== false );
 
 /**
  * Whether the server software is Nginx or something else
+ *
  * @global bool $is_nginx
  */
-$is_nginx = (strpos($_SERVER['SERVER_SOFTWARE'], 'nginx') !== false);
+$is_nginx = ( strpos( $_SERVER['SERVER_SOFTWARE'], 'nginx' ) !== false );
 
 /**
  * Whether the server software is IIS or something else
+ *
  * @global bool $is_IIS
  */
-$is_IIS = !$is_apache && (strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false || strpos($_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer') !== false);
+$is_IIS = !$is_apache && ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) !== false || strpos( $_SERVER['SERVER_SOFTWARE'], 'ExpressionDevServer' ) !== false );
 
 /**
  * Whether the server software is IIS 7.X or greater
+ *
  * @global bool $is_iis7
  */
 $is_iis7 = $is_IIS && intval( substr( $_SERVER['SERVER_SOFTWARE'], strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS/' ) + 14 ) ) >= 7;
 
 /**
- * Test if the current browser runs on a mobile device (smart phone, tablet, etc.)
+ * Test if the current browser runs on a mobile device ( smart phone, tablet, etc.)
  *
- * @since 3.4.0
- * 
+ * @since  Previous 3.4.0
  * @return bool
  */
 function wp_is_mobile() {
-	if ( empty($_SERVER['HTTP_USER_AGENT']) ) {
+
+	if ( empty( $_SERVER['HTTP_USER_AGENT'] ) ) {
 		$is_mobile = false;
-	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false // many mobile devices (all iPhone, iPad, etc.)
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
-		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false ) {
+
+	} elseif ( strpos( $_SERVER['HTTP_USER_AGENT'], 'Mobile' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'Android' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'Silk/' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'Kindle' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'BlackBerry' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera Mini' ) !== false
+		|| strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera Mobi' ) !== false ) {
 			$is_mobile = true;
 	} else {
 		$is_mobile = false;
@@ -142,9 +183,129 @@ function wp_is_mobile() {
 	/**
 	 * Filters whether the request should be treated as coming from a mobile device or not.
 	 *
-	 * @since 4.9.0
-	 *
+	 * @since Previous 4.9.0
 	 * @param bool $is_mobile Whether the request is from a mobile device or not.
 	 */
 	return apply_filters( 'wp_is_mobile', $is_mobile );
 }
+
+/**
+ * Browser & OS classes
+ *
+ * @since  1.0.0
+ * @param  mixed $classes
+ * @return mixed Returns the filtered classes. String back end, array front end.
+ */
+function app_agent_body_class( $classes ) {
+
+	global
+	$pagenow,
+	$is_lynx,
+	$is_gecko,
+	$is_winIE,
+	$is_macIE,
+	$is_opera,
+	$is_NS4,
+	$is_safari,
+	$is_chrome,
+	$is_iphone,
+	$is_IE,
+	$is_edge,
+	$is_apache,
+	$is_IIS,
+	$is_iis7,
+	$is_nginx;
+
+	// Backend classes.
+	if ( is_admin() ) {
+
+		if ( $is_lynx ) {
+			$classes = 'browser-lynx';
+		} elseif ( $is_gecko ) {
+			$classes = 'browser-gecko';
+		} elseif ( $is_opera ) {
+			$classes = 'browser-opera';
+		} elseif ( $is_NS4 ) {
+			$classes = 'browser-ns4';
+		} elseif ( $is_safari ) {
+			$classes = 'browser-safari';
+		} elseif ( $is_chrome ) {
+			$classes = 'browser-chrome';
+		} elseif ( $is_edge ) {
+			$classes = 'browser-edge';
+		} elseif ( $is_IE ) {
+			$classes = 'browser-ie';
+
+			if ( preg_match( '/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version ) ) {
+				$classes = 'browser-ie' . $browser_version[1];
+			}
+
+		} else {
+			$classes = 'browser-unknown';
+		}
+
+		if ( $is_iphone ) {
+			$classes = 'browser-iphone';
+		}
+
+		// Space between browser class and OS class.
+		$classes .= ' ';
+
+		if ( stristr( $_SERVER['HTTP_USER_AGENT'], 'mac' ) ) {
+			$classes .= 'os-osx';
+		} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'], 'linux' ) ) {
+			$classes .= 'os-linux';
+		} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'], 'windows' ) ) {
+			$classes .= 'os-windows';
+		} else {
+			$classes .= 'os-unknown';
+		}
+
+	// Frontend classes.
+	} else {
+
+		if ( $is_lynx ) {
+			$classes[] = 'browser-lynx';
+		} elseif ( $is_gecko ) {
+			$classes[] = 'browser-gecko';
+		} elseif ( $is_opera ) {
+			$classes[] = 'browser-opera';
+		} elseif ( $is_NS4 ) {
+			$classes[] = 'browser-ns4';
+		} elseif ( $is_safari ) {
+			$classes[] = 'browser-safari';
+		} elseif ( $is_chrome ) {
+			$classes[] = 'browser-chrome';
+		} elseif ( $is_edge ) {
+			$classes[] = 'browser-edge';
+		} elseif ( $is_Ie ) {
+			$classes[] = 'browser-ie';
+
+			if ( preg_match( '/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version ) ) {
+				$classes[] = 'browser-ie' . $browser_version[1];
+			}
+
+		} else {
+			$classes[] = 'browser-unknown';
+		}
+
+		if ( $is_iphone ) {
+			$classes[] = 'browser-iphone';
+		}
+
+		if ( stristr( $_SERVER['HTTP_USER_AGENT'], 'mac' ) ) {
+			$classes[] .= 'os-osx';
+		} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'], 'linux' ) ) {
+			$classes[] .= 'os-linux';
+		} elseif ( stristr( $_SERVER['HTTP_USER_AGENT'], 'windows' ) ) {
+			$classes[] .= 'os-windows';
+		} else {
+			$classes[] .= 'os-unknown';
+		}
+	}
+
+	// Return the filtered classes.
+	return $classes;
+}
+add_filter( 'admin_body_class', 'app_agent_body_class' );
+add_filter( 'body_class', 'app_agent_body_class' );
