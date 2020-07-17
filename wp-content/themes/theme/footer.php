3cb34d9b522b2c083ac5e01db60f1d8f7032cc6e
antibrand <antibrand@ccdzine.com>
2020-07-17 00:39:26 -0800

    Updated default theme from its repository.

--- wp-content/themes/theme/footer.php
+++ wp-content/themes/theme/footer.php
@@ -10,20 +10,18 @@
 // Get the site name.
 $site_name = esc_attr( get_bloginfo( 'name' ) );
 
-// Copyright HTML.
-$copyright = sprintf(
-	'<p class="copyright-text" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">&copy; <span class="screen-reader-text">%1s</span><span itemprop="copyrightYear">%2s</span> <span itemprop="copyrightHolder">%3s.</span> %4s.</p>',
-	esc_html__( 'Copyright ', 'antibrand' ),
+// Footer message.
+$message = sprintf(
+	'<p class="footer-message" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">%1s %2s</p>',
 	get_the_time( 'Y' ),
-	$site_name,
-	esc_html__( 'All rights reserved', 'antibrand' )
+	$site_name
 ); ?>
 
 	</div>
 
 	<footer id="colophon" class="site-footer">
 		<div class="footer-content global-wrapper footer-wrapper">
-			<?php echo $copyright; ?>
+			<?php echo apply_filters( 'ab_theme_footer_message', $message ); ?>
 		</div>
 	</footer>
 </div>
