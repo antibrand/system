feac9a3a609bf34b2c9d61fbb27770a820c26dd9
antibrand <antibrand@ccdzine.com>
2020-07-20 20:26:51 -0800

    Alias extension: constants.

--- app-extend/extensions/alias/constants.php
+++ app-extend/extensions/alias/constants.php
@@ -11,26 +11,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Debug mode.
+/**
+ * Debug mode
+ *
+ * @since 1.0.0
+ * @var   boolean
+ */
 if ( ! defined( 'WP_DEBUG' ) && defined( 'APP_DEBUG' ) ) {
 	define( 'WP_DEBUG', APP_DEBUG );
 } else {
 	define( 'WP_DEBUG', false );
 }
 
-// Installing.
+/**
+ * Installing
+ *
+ * @since 1.0.0
+ * @var   boolean
+ */
 if ( ! defined( 'WP_INSTALLING' ) && defined( 'APP_INSTALLING' ) ) {
 	define( 'WP_INSTALLING', APP_INSTALLING );
 }
 
-// Languages directory.
+/**
+ * Plugins directory
+ *
+ * Check for definition with a value.
+ *
+ * @since 1.0.0
+ * @var   string Returns the path to the plugins directory.
+ */
 if ( ! defined( 'WP_PLUGIN_DIR' ) && ( defined( 'APP_PLUGIN_DIR' ) && APP_PLUGIN_DIR ) ) {
 	define( 'WP_PLUGIN_DIR', APP_PLUGIN_DIR );
 } else {
 	define( 'WP_PLUGIN_DIR', ABSPATH . 'app-extend/plugins' );
 }
 
-// Languages directory.
+/**
+ * Languages directory
+ *
+ * Check for definition with a value.
+ *
+ * @since 1.0.0
+ * @var   string Returns the path to the languages directory.
+ */
 if ( ! defined( 'WP_LANG_DIR' ) && ( defined( 'APP_LANG_DIR' ) && APP_LANG_DIR ) ) {
 	define( 'WP_LANG_DIR', APP_LANG_DIR );
 } else {
@@ -38,21 +62,37 @@ if ( ! defined( 'WP_LANG_DIR' ) && ( defined( 'APP_LANG_DIR' ) && APP_LANG_DIR )
 }
 
 /**
- * Network constants
+ * Allow network
  *
  * @since 1.0.0
+ * @var   boolean
  */
-
-// Allow network.
 if ( ! defined( 'WP_ALLOW_MULTISITE' ) && defined( 'APP_ALLOW_NETWORK' ) ) {
 	define( 'WP_ALLOW_MULTISITE', APP_ALLOW_NETWORK );
 } else {
 	define( 'WP_ALLOW_MULTISITE', false );
 }
 
-// Network enabled.
+/**
+ * Network enabled
+ *
+ * @since 1.0.0
+ * @var   boolean
+ */
 if ( ! defined( 'MULTISITE' ) && defined( 'APP_NETWORK' ) ) {
 	define( 'MULTISITE', APP_NETWORK );
 } else {
 	define( 'MULTISITE', false );
+}
+
+/**
+ * Network admin
+ *
+ * @since 1.0.0
+ * @var   boolean
+ */
+if ( ! defined( 'WP_NETWORK_ADMIN' ) && defined( 'APP_NETWORK_ADMIN' ) ) {
+	define( 'WP_NETWORK_ADMIN', APP_NETWORK_ADMIN );
+} else {
+	define( 'WP_NETWORK_ADMIN', false );
 }
