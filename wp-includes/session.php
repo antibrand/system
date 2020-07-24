0cdeac40b36e9c0af4a18eecfa23f165c186d602
antibrand <antibrand@ccdzine.com>
2020-07-23 17:30:40 -0800

    Moved files to app-includes.

--- wp-includes/session.php
+++ wp-includes/session.php
@@ -8,4 +8,4 @@
 _deprecated_file( basename( __FILE__ ), '4.7.0' );
 
 require_once( ABSPATH . APPINC . '/classes/includes/class-app-session-tokens.php' );
-require_once( ABSPATH . WPINC . '/class-wp-user-meta-session-tokens.php' );
+require_once( ABSPATH . APPINC . '/classes/includes/class-app-user-meta-session-tokens.php' );
