25916fbaf3c3aedc912d21330413269cc24e7169
antibrand <antibrand@ccdzine.com>
2020-08-02 09:55:42 -0800

    Text edit.

--- wp-admin/options-discussion.php
+++ wp-admin/options-discussion.php
@@ -219,7 +219,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 							<p><label for="comment_max_links"><?php printf(__( 'Hold a comment in the queue if it contains %s or more links. (A common characteristic of comment spam is a large number of hyperlinks.)' ), '<input name="comment_max_links" type="number" step="1" min="0" id="comment_max_links" value="' . esc_attr( get_option( 'comment_max_links' ) ) . '" class="small-text" />' ); ?></label></p>
 
-							<p><label for="moderation_keys"><?php _e( 'When a comment contains any of these words in its content, name, URL, email, or IP address, it will be held in the <a href="edit-comments.php?comment_status=moderated">moderation queue</a>. One word or IP address per line. It will match inside words, so &#8220;fruit&#8221; will match &#8220;grapefruit&#8221;.' ); ?></label></p>
+							<p><label for="moderation_keys"><?php _e( 'When a comment contains any of these words in its content, name, URL, email, or IP address, it will be held in the <a href="edit-comments.php?comment_status=moderated">moderation queue</a>. One word or IP address per line. It will match inside words, so &#8220;central&#8221; will match &#8220;decentralize&#8221;.' ); ?></label></p>
 							<p>
 								<textarea name="moderation_keys" rows="10" cols="50" id="moderation_keys" class="large-text code"><?php echo esc_textarea( get_option( 'moderation_keys' ) ); ?></textarea>
 							</p>
