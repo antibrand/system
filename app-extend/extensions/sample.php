f10abae0d68a4745093d735c55a4beba5740e671
antibrand <antibrand@ccdzine.com>
2020-08-12 15:11:36 -0800

    Sample extension. Used for testing.

---
+++ app-extend/extensions/sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Sample
+ *
+ * @package Sample
+ * @version 1.0.0
+ *
+ * Plugin Name: Sample
+ * Description: Sample extension header.
+ * Plugin URI: https://example.com/
+ * Author: Anonymous
+ * Tags: Example, Development
+ */
+
+// Do nothing!
