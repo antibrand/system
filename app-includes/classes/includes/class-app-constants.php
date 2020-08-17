e773f011c475317d44e49733840fa64b150531c5
antibrand <antibrand@ccdzine.com>
2020-08-17 08:21:32 -0800

    App constants class.

--- app-includes/classes/includes/class-app-constants.php
+++ app-includes/classes/includes/class-app-constants.php
@@ -5,16 +5,18 @@
  * Methods to define constants.
  *
  * @package App_Package
- * @subpackage Includes\Config_Install
+ * @subpackage Includes
  * @since 1.0.0
  */
 
 namespace AppNamespace\Includes;
 
-// Get the default system constants.
-require_once( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/app-constants.php' );
+// Get the default system constants from the system root directory.
+if ( file_exists( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/app-constants.php' ) ) {
+	require_once( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/app-constants.php' );
+}
 
 class App_Constants {
 
-	
+
 }
