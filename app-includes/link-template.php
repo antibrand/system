625305bd642e4db35e0637cc280d3ab1fb2cca90
antibrand <antibrand@ccdzine.com>
2020-08-20 19:35:16 -0800

    Revert "Views constants."
    This reverts commit b670b3250e55da3d9eecc61b564467a83e427bbe.

--- app-includes/link-template.php
+++ app-includes/link-template.php
@@ -3211,7 +3211,7 @@ function includes_url( $path = '', $scheme = null ) {
 function content_url( $path = '' ) {
 
 	if ( defined( 'APP_VIEWS' ) ) {
-		$url = set_url_scheme( get_option( 'siteurl' ) . '/' . APP_VIEWS_DIR );
+		$url = set_url_scheme( get_option( 'siteurl' ) . '/' . APP_VIEWS );
 	} else {
 		$url = set_url_scheme( get_option( 'siteurl' ) . '/app-views' );
 	}
