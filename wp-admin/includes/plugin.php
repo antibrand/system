6d35ad6080235e8f72fa4629d30bf06ee648de27
antibrand <antibrand@ccdzine.com>
2020-08-27 19:45:00 -0800

    Comment out deprecated file line.

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -10,7 +10,7 @@
 // Get definitions & variables from the patch directory index.
 require_once( dirname( dirname( __FILE__ ) ) . '/index.php' );
 
-_deprecated_file( basename( __FILE__ ), '1.0.0', ADMIN_PATCH_INC_DIR . '/plugin.php' );
+// _deprecated_file( basename( __FILE__ ), '1.0.0', ADMIN_PATCH_INC_DIR . '/plugin.php' );
 
 // Get plugin.php from its new location.
 if ( file_exists( APP_INC_PATH . '/backend/plugin.php' ) ) {
