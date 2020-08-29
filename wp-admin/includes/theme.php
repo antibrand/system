c688c7923ec5ff0dbc57442d69f35f531b1cb41e
antibrand <antibrand@ccdzine.com>
2020-08-29 13:24:13 -0800

    Admin alias: theme, fix plugin.

--- wp-admin/includes/theme.php
+++ wp-admin/includes/theme.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin patch
+ * Theme patch
  *
  * @package App_Package
  * @subpackage Directory_Patch
@@ -10,7 +10,7 @@
 // Get definitions & variables from the patch directory index.
 require_once( dirname( dirname( __FILE__ ) ) . '/index.php' );
 
-// Get plugin.php from its new location.
-if ( file_exists( APP_INC_PATH . '/backend/plugin.php' ) ) {
-	require_once( APP_INC_PATH . '/backend/plugin.php' );
+// Get theme.php from its new location.
+if ( file_exists( APP_INC_PATH . '/backend/theme.php' ) ) {
+	require_once( APP_INC_PATH . '/backend/theme.php' );
 }
