11e70566047c9c3cf3dda91119a85f2c82fb90f2
antibrand <antibrand@ccdzine.com>
2020-08-30 09:36:25 -0800

    Docblock.

--- wp-admin/includes/index.php
+++ wp-admin/includes/index.php
@@ -2,6 +2,10 @@
 /**
  * Directory patch
  *
+ * This directory exists only as a patch for plugins
+ * that require files that had been in a directory
+ * with the same name.
+ *
  * @package App_Package
  * @subpackage Directory_Patch
  * @since 1.0.0
